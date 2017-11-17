576f20a3c3bacc923b9d2eb0e7e6b256f038935d
adithya1894 <adithya1894@gmail.com>
2017-11-17 03:51:19 -0500

    Added the Interface Methods pwd() and File_check()

--- src/RmiInterface.java
+++ src/RmiInterface.java
@@ -2,14 +2,34 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface RmiInterface extends Remote {
-
+    /**
+     * Method to Echo the input sent by the user
+     * @param input Takes the user input as argument
+     * @return returns a string which is same as the input sent by the user
+     * @throws RemoteException
+     */
     String echo(String input) throws RemoteException;
 
     /**
      * Metohd to sort the list elements, takes the input as an array
-     * @param list_input
-     * @return
+     * @param list_input Array of integers
+     * @return the sorted list
      * @throws RemoteException
      */
     int[] sort_list(int[] list_input) throws RemoteException;
+
+    /**
+     * Returns the current working directory of the server
+     * @return
+     * @throws RemoteException when the method receives an error from the server
+     */
+    String pwd() throws RemoteException;
+
+    /**
+     * Checks if the entered filename is present at the server
+     * @param file_name, the file which you want to check
+     * @return true if the searched file is present, if not False
+     * @throws RemoteException when the method receives an Exception instead of result
+     */
+    String file_check(String file_name) throws RemoteException;
 }
