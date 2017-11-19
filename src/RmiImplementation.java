0df86e3f94128e5782d539c6aa6c0808a9b3b484
adithya1894 <adithya1894@gmail.com>
2017-11-19 18:41:56 -0500

    Added Overridden Methods

---
+++ src/RmiImplementation.java
@@ -0,0 +1,30 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class RmiImplementation extends UnicastRemoteObject implements RmiInterface{
+
+
+    public RmiImplementation() throws RemoteException{
+        super();
+
+    }
+    @Override
+    public String echo(String input) throws RemoteException {
+        return input;
+    }
+
+    @Override
+    public int[] sort_list(int[] list_input) throws RemoteException {
+        return new int[0];
+    }
+
+    @Override
+    public String pwd() throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public String file_check(String file_name) throws RemoteException {
+        return null;
+    }
+}
