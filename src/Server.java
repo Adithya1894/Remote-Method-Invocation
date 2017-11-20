98a2e8bb6ac62bac58981f87fadc862abd283681
adithya1894 <adithya1894@gmail.com>
2017-11-19 19:14:15 -0500

    changed the name in the Rebind method to "server"

--- src/Server.java
+++ src/Server.java
@@ -9,7 +9,7 @@ public class Server {
 
     public static void main(String args[]) throws RemoteException, MalformedURLException {
         RmiImplementation stub = new RmiImplementation();
-        Naming.rebind("Implementation", stub);
+        Naming.rebind("server", stub);
         System.out.println("Server Started: 6060");
 
     }
