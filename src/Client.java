083167cc6bf977cc154b14e313cbe4744252472e
adithya1894 <adithya1894@gmail.com>
2017-11-19 19:51:43 -0500

    Added exception handling and reading input from user

--- src/Client.java
+++ src/Client.java
@@ -2,19 +2,34 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Scanner;
 
 public class Client {
 
     public static void main(String args[]) throws RemoteException, MalformedURLException, NotBoundException {
 
-        String val = "Hello";
+        String val;
 
-        RmiInterface obj = (RmiInterface) Naming.lookup("server");
 
-        String result = obj.echo(val);
+        try {
+            RmiInterface obj = (RmiInterface) Naming.lookup("server");
+            int i = 0;
+            while( i < 10)
+            {
 
-        System.out.println("result from the server is: " + result);
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter the input: \n");
+                val = sc.nextLine();
 
+                String result = obj.echo(val);
+                System.out.println("result from the server is: " + result);
+                i++;
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("Exception occured");
+        }
 
 
     }
