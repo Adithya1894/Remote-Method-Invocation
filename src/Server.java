1097d50087e922e1ac580d907d78cc2e3fadd614
adithya1894 <adithya1894@gmail.com>
2017-11-29 00:51:25 -0500

    Dead code Removed

--- src/Server.java
+++ src/Server.java
@@ -10,15 +10,14 @@ public class Server {
 
     public static void main(String args[]) throws RemoteException, MalformedURLException {
 
-        //System.setSecurityManager(new SecurityManager());
+
 
         try {
             /**
              * Binding the Server with the functionality present at the server
              */
 
-            //Registry register= LocateRegistry.createRegistry(5050);
-            //RmiInterface stub = new RmiImplementation();
+
             String name = "server";
             RmiImplementation stub = new RmiImplementation(name);
 
