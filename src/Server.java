e73b2125e059ce7699bda78be1faf7ffce5fc90a
adithya1894 <adithya1894@gmail.com>
2018-03-03 19:11:24 -0500

    added Role Based Access Control

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Proxy;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
@@ -11,20 +12,46 @@ public class Server {
     public static void main(String args[]) throws RemoteException, MalformedURLException {
 
 
-
-        try {
-            /**
+/**
              * Binding the Server with the functionality present at the server
              */
 
 
-            String name = "server";
-            RmiImplementation stub = new RmiImplementation(name);
+            //System.setSecurityManager(new SecurityManager());
+		
+		try {
+			System.out.println("Creating a Server!");
+			
+			// Location of Server
+			String name = "//127.0.0.1:2012/server";
+			
+			System.out.println("Server: Binding it to name: " + name);
+			
+                        
+                        
+                        RmiInterface assignment = (RmiInterface)Proxy.newProxyInstance(RmiInterface.class.getClassLoader(),
+	                new Class<?>[] {RmiInterface.class},
+	                new AuthorizationInvocationHandler(new RmiImplementation()));
 
-            Naming.rebind(name, stub);
-
-            System.out.println("Server Started");
-        }
+//			RmiInterface assignment = (RmiInterface)Proxy.newProxyInstance(RmiInterface.class.getClassLoader(),
+//	                new Class<?>[] {RmiInterface.class},
+//	                new AuthorizationInvocationHandler(new RmiImplementation()));
+//						
+			// Binds the Server to the RMI Service.
+			Naming.rebind(name, assignment);
+			
+			System.out.println("Server Ready!");
+		}            
+            
+            
+            
+//            String name = "server";
+//            RmiImplementation stub = new RmiImplementation(name);
+//
+//            Naming.rebind(name, stub);
+//
+//            System.out.println("Server Started");
+//        }
         catch(Exception e)
         {
             e.printStackTrace();
